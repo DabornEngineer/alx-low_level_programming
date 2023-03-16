14c5a0d8d9bee3a6d1ec2cf0085d3b1bf364915c
Daborn1 <orjimiracle870@gmail.com>
2023-03-16 15:06:24 -0700

    A c file that prints with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The program execution begins at the main function
+ * Return: always zero for success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
