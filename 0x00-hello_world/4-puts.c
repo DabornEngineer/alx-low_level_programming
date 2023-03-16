b72d4896fa6ca70adca20324e573579934cf558a
Daborn1 <orjimiracle870@gmail.com>
2023-03-16 14:58:29 -0700

    A c file that prints Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program execution begins
+ * Return: always zero(0) for success
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
