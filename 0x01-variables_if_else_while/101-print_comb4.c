06e39371f708f1d8d2dee94cec05414e821f463d
Daborn1 <orjimiracle870@gmail.com>
2023-03-17 18:42:18 -0700

    prints all the combination of 3 digits wthout repetition

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Program execution begings
+ * num_1: stores the value of the first digit
+ * num_2: stores the value of the second digit
+ * num_3: stores the value of the third digit
+ */
+int main(void)
+{
+	int num_1, num_2, num_3;
+
+	for (num_1 = 0; num_1 < 8; num_1++)
+	{
+		for (num_2 = num_1 + 1; num_2 < 9; num_2++)
+		{
+			for (num_3 = num_1 + 2; num_3 < 10; num_3++)
+			{
+				putchar(num_1 + '0');
+				putchar(num_2 + '0');
+				putchar(num_3 + '0');
+				if (num_1 == 7 && num_2 == 8 && num_3 == 9)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
