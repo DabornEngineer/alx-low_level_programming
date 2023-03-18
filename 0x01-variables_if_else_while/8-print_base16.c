557cc8ac45a48df7b6c9e7bc422ef86b908c9b03
Daborn1 <orjimiracle870@gmail.com>
2023-03-17 17:23:38 -0700

    prints hex value

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function execution begins
+ * store_hex: A variable that stores the value of the hexadecimal
+ * return: Always zero for success
+ */
+int main(void)
+{
+	int store_hex;
+
+	for (store_hex = '0'; store_hex <= '9'; store_hex++)
+		putchar(store_hex);
+	for (store_hex = 'a'; store_hex <= 'f'; store_hex++)
+		putchar(store_hex);
+	putchar('\n');
+
+	return (0);
+}
