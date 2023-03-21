356d6e52b32f0db46562234eea847711df13765c
Daborn1 <orjimiracle870@gmail.com>
2023-03-21 14:39:41 -0700

    adjustment

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,22 @@
 /**
  * _islower - prints the lowercase of a passed argument
  * @c: argument that collect the character
+ * lowercase: gets the lowercase
  * Return: 1 for lowercase and 0 for uppercase
  */
 int _islower(int c)
 {
-	if (c == 'a' && c <= 'z')
+	int lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 	{
-		return (1);
-	}
-	else
-	{
-		return (0);
+		if (c == lowercase)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
 	}
 }
