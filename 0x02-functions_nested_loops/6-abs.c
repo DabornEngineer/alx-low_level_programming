e761505e724fcf5aabd762285ca72619867813c4
Daborn1 <orjimiracle870@gmail.com>
2023-03-21 15:55:49 -0700

    abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _abs - prints the basolute value of a number
+ * @n: collects the interger
+ * Return: value of abs
+ */
+int _abs(int n)
+{
+	return (sqrt(n * n));
+}
