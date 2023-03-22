368bbc35c599f618df3b84779c121b47a6f3e03c
Daborn1 <orjimiracle870@gmail.com>
2023-03-21 17:20:29 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints 9 times table
+ * multi1: value for multiplication
+ * multi2: value for multiplication
+ * result: the answer of the multiplication
+ */
+void times_table(void);
+{
+	int multi1, multi2;
+	int result;
+
+	for (multi1 = 0; multi1 < 10; multi1++)
+	{
+		for (multi2 = 0; multi2 < 10; multi2++)
+		{
+			result = multi1 * multi2;
+			_putchar(result + '0');
+			_putchar(',');
+			_putchar('\n');
+			if (multi2 == 9)
+				_putchar('\n');
+		}
+	}
+}
