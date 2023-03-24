fbf1c322cefa9a01c36ca95658cb669bfeab16c6
Daborn1 <orjimiracle870@gmail.com>
2023-03-24 15:34:01 -0700

    correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,23 @@ int main(void)
 	{
 		if (print_num % 3 == 0 && print_num % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (print_num % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (print_num % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", print_num);
+			printf("%d", print_num);
+		}
+		if (print_num != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
