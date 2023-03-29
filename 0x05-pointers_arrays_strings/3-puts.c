0488d171070df7cee67c13033d0ee2ec99f5bf86
Daborn1 <orjimiracle870@gmail.com>
2023-03-28 17:11:34 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - the function name
+ * @str: The string
+ */
+
+void _puts(char *str)
+{
+	int c;
+
+	while (*(str + c) != '\0')
+	{
+		_putchar(*(str + c));
+		c++;
+	}
+}
