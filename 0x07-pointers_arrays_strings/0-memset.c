0785047ac6a2d8e394a29c343b1bb1885b0dda7c
Daborn1 <orjimiracle870@gmail.com>
2023-04-03 16:08:52 -0700

    set memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - the function name
+ * @s: the string
+ * @b: the character to change to
+ * @n: the integer
+ * Return: the string scanged
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int count = 0;
+
+	for (count = 0; count < n; count++)
+	{
+		*(s + count) = b;
+	}
+	return (s);
+}
