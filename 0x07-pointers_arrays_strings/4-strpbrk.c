51345db32dad56269576afe4794f8191b8323277
Daborn1 <orjimiracle870@gmail.com>
2023-04-03 19:16:29 -0700

    prints

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <typedef.h>
+
+/**
+ * _strpbrk - the function name
+ * @s: the string
+ * @accept: the string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int len_s = 0, len_acc = 0;
+	int i, j;
+
+	while (*(s + len_s) != '\0')
+		len_s++;
+	while (*(accept + len_acc) != '\0')
+		len_acc++;
+	for (i = 0; i <= len_s; i++)
+	{
+		for (j = 0; j <= len_acc; j++)
+		{
+			if (*(s + i) == *(accept + j))
+					return (s + i);
+		}
+	}
+	return (NULL);
+}
